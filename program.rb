21e64a8555cb140fb243e76af41620590990664e
Adam Plansky <adamplansky@gmail.com>
2015-10-24 19:20:04 +0200

    upravy

--- program.rb
+++ program.rb
@@ -2,12 +2,12 @@ def get_games(file_name, nickname)
   hand_text = ""
   #line_cnt = 0
   File.open(File.dirname(__FILE__) + '/' + file_name, 'r').each_line do |line|
-
+    hand_text += line
     if line == "\r\n" || line == "\n"
       puts hand_text if hand_text.include? nickname
       hand_text = ""
     end
-    hand_text += line
+
     #line_cnt += 1
   end
 end
