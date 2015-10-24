fb06b0790604bc9c3893eda1648a02d854ab3728
Adam Plansky <adamplansky@gmail.com>
2015-10-24 18:58:38 +0200

    rubocop fix

--- program.rb
+++ program.rb
@@ -1,20 +1,16 @@
 def get_games(file_name, nickname)
-  hand = false
-  hand_text = ""
+  hand_text = ''
   line_cnt = 0
-  File.open(File.dirname(__FILE__)+'/'+file_name , "r").each_line do |line|
-    hand = true if line.include? "PokerStars Hand"
-    if line == "\r\n" || line == "\n"
+  File.open(File.dirname(__FILE__) + '/' + file_name, 'r').each_line do |line|
+    if line == '\r\n' || line == '\n'
       puts hand_text if hand_text.include? nickname
-      hand_text = ""
+      hand_text = ''
     end
-    hand_text +=  line
-
+    hand_text += line
     line_cnt += 1
   end
 end
 
-
 file_name = ARGV[0]
 nick_name = ARGV[1]
-get_games(file_name,nick_name)
+get_games(file_name, nick_name)
