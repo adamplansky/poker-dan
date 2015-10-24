563b0111abf6e96b4162e631d44793b4b276d996
Adam Plansky <adamplansky@gmail.com>
2015-10-24 19:14:59 +0200

    windows fix

--- program.rb
+++ program.rb
@@ -3,7 +3,7 @@ def get_games(file_name, nickname)
   #line_cnt = 0
   File.open(File.dirname(__FILE__) + '/' + file_name, 'r').each_line do |line|
     hand_text += line
-    if line == "\r\n" || line == '\n'
+    if line == "\r\n" || line == "\n"
       puts hand_text if hand_text.include? nickname
       hand_text = ""
     end
